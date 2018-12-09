b74fec3bce0bd5e9833d97945a9571d417ae60ac
Huiyan820 <huiyan.chen.2015@smu.edu.sg>
2018-12-09 22:10:23 +0800

    fixed update api

--- lastMileBackend/src/main/java/com/lastmileapp/Controller/DriverController.java
+++ lastMileBackend/src/main/java/com/lastmileapp/Controller/DriverController.java
@@ -46,7 +46,7 @@ public class DriverController {
     @RequestMapping(value="update", method=RequestMethod.POST)
     public HashMap<String,Boolean> update(@RequestParam("longitude") double longitude, @RequestParam("latitude") double latitude, @RequestParam("plateNum") String plateNum) {
         HashMap<String, Boolean> result = new HashMap<>();
-        result.put("plateNum",driverService.updateDriverLocation(longitude,latitude,plateNum));
+        result.put("status",driverService.updateDriverLocation(longitude,latitude,plateNum));
         return result;
     }
 
